d100f7a6496d7aab12a52234ee456f65878df27b
bruno santos <bruno4t@hotmail.com>
2022-11-28 17:56:34 -0300

    criação do metodo post e FindbyId

--- src/main/java/com/bruno/crudspring/controller/CourseController.java
+++ src/main/java/com/bruno/crudspring/controller/CourseController.java
@@ -2,10 +2,9 @@ package com.bruno.crudspring.controller;
 
 import com.bruno.crudspring.model.Course;
 import com.bruno.crudspring.repository.CourseRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +22,18 @@ public class CourseController {
     @GetMapping
     public @ResponseBody List<Course> list(){
         return courseRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id){
+        return courseRepository.findById(id)
+               .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course){
+        return courseRepository.save(course);
     }
 }
